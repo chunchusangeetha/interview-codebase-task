e30aa8419b2b86140689a0eadcc1d5a288901aa4
chunchusangeetha <116276304+chunchusangeetha@users.noreply.github.com>
2025-05-07 07:08:54 +0000

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
+<<<<<<< HEAD
+=======
+import { useEffect } from "react";
+import { Outlet, useLocation } from "react-router-dom";
+import { MantineProvider } from "@mantine/core";
+import { theme } from "./theme";
+import "./App.scss";
+>>>>>>> 55a1489 (code done)
 import { Routes, Route, Navigate } from "react-router-dom";
 import SignIn from "./pages/auth/SignIn";
 import SignUp from "./pages/auth/SignUp";
 import ResourceList from "./pages/auth/ResourceList";
+<<<<<<< HEAD
 import ResourceDetail from "./pages/auth/ResourceDetail";
+=======
+import ResourceDetail from "./pages/auth/ResoureDetail"
+>>>>>>> 55a1489 (code done)
 
 const App = () => {
   return (
@@ -17,3 +29,7 @@ const App = () => {
 };
 
 export default App;
+<<<<<<< HEAD
+=======
+
+>>>>>>> 55a1489 (code done)
