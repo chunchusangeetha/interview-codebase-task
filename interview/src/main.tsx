11b296eed46376533633848a0a5ae5f406491446
chunchusangeetha <116276304+chunchusangeetha@users.noreply.github.com>
2025-05-08 06:58:43 +0530

    Delete interview directory

--- interview/src/main.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <h1>Hello, Vite!</h1>
-  </React.StrictMode>
-);
